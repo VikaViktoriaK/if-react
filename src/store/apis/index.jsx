4c44c375c32e62702f1ba5aab0f239f7607e8464
viktoria kolosova <vikakolosova04@gmail.com>
2025-04-01 18:32:22 +0300

    lesson-40: added changes according to comments

---
+++ src/store/apis/index.jsx
@@ -0,0 +1,3 @@
+import { useSearchHotelsQuery} from './hotels/searchHotels.endpoint';
+import { useGetPopularHotelsQuery } from './hotels/popularHotels.endpoint';
+import {hotelsApi} from './hotels/hotels.api';
